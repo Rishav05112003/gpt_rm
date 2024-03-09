f2d6376c6f2ea509a4d79ff322cbd7c772fcb2f0
Rishav05112003 <rishavdeep.skills@gmail.com>
2024-03-09 12:28:30 +0530

    changes in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,23 @@ const Navbar = () => {
   return (
     <div className='gpt3__navbar'>
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links-logo">
+        <div className="gpt3__navbar-links_logo">
             <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+
+
     </div>
   );
 };
