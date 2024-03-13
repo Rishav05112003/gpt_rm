268b5be375be5dcb25baa743c5ce6232bffa40a8
Rishav05112003 <rishavdeep.skills@gmail.com>
2024-03-13 15:45:43 +0530

    navbar finished,header started

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
+    
     <div className='gpt3__navbar'>
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
@@ -21,7 +24,26 @@ const Navbar = () => {
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/> 
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-centre">
+            <div className="gpt3__navbar-menu-container-links">
+              <p><a href='#home'>Home</a></p>
+              <p><a href="#wgpt3">What is GPT3?</a></p>
+              <p><a href="#possibility">Open AI</a></p>
+              <p><a href="#features">Case Studies</a></p>
+              <p><a href="#blog">Library</a></p>
+            </div>
+            <div className="gpt3__navbar-menu-container-links-sign">
+              <p>Sign In</p>
+              <button type='button'>Sign In</button>
+            </div>
 
+          </div>
+        )}
+      </div>
 
     </div>
   );
