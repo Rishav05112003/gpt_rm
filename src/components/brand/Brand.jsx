5c8a609ddbdfbb41a036b10645ccc210b25173c4
Rishav05112003 <rishavdeep.skills@gmail.com>
2024-03-16 16:50:12 +0530

    brand portion made

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {google,
+  slack,
+  atlassian,
+  shopify} from './import';
 import './brand.css'
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div  className="gpt3__brand">
+      <div>
+        <img src = {slack}/>
+      </div>
+      <div>
+        <img src = {google}/>
+      </div>
+      <div>
+        <img src = {atlassian}/>
+      </div>
+      <div>
+        <img src = {shopify}/>
+      </div>
     </div>
   )
 };
